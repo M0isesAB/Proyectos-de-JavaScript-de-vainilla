9e580c74751cf5b608d04ce736102ee39b18a0ad
M0isesAB <114767403+M0isesAB@users.noreply.github.com>
2022-10-03 18:10:22 -0500

    Add files via upload

---
+++ 04-Navbar/app.js
@@ -0,0 +1,19 @@
+
+
+const navToogle=document.querySelector('.nav-toggle');
+const links=document.querySelector('.links');
+
+/* const add=document.querySelector('.add');
+const remove=document.querySelector('.remove');
+const toggle=document.querySelector('.toggle'); */
+
+
+navToogle.addEventListener("click",function(){
+   /*  if(links.classList.contains('show-links')){
+        console.log('hello');
+        links.classList.remove('show-links');
+    }else{    links.classList.add('show-links');}
+  */
+    links.classList.toggle('show-links');
+
+})
